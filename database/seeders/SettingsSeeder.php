fd8add8c35fc9f53195006ee43caf396df9c0bab
zahir-ullah <zahirullah2099@gmail.com>
2026-08-14 10:50:56 +0500

    removed factories

--- database/seeders/SettingsSeeder.php
+++ database/seeders/SettingsSeeder.php
@@ -11,10 +11,10 @@ class SettingsSeeder extends Seeder
     {
         $defaults = [
             'shop_name' => 'My Tailor Shop',
-            'shop_phone' => '',
-            'shop_email' => '',
-            'shop_address' => '',
-            'shop_description' => '',
+            'shop_phone' => '03001234567',
+            'shop_email' => 'info@mytailorshop.com',
+            'shop_address' => '123 Tailor Street, City',
+            'shop_description' => 'A beautiful tailor shop offering custom clothing services.',
         ];
 
         foreach ($defaults as $key => $value) {
